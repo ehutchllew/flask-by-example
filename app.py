f346a448711ef3d0ec76493dc2ab6ab3ca123aaa
Evan H L <ehutch7@gmail.com>
2020-10-02 20:44:49 -0400

    Testing out some url params.

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return "Hello world!"
+@app.route('/<name>')
+def hello(name):
+    return f"Hello {name}!"
 
 if __name__ == '__main__':
     app.run()
